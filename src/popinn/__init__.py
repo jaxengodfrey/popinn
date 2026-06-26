583a69f5a611824db02a2dbf8dc0283656b469bd
Jaxen Godfrey <jaxeng@uoregon.edu>
2026-06-26 13:48:57 -0700

    add FixedWeights to __init__

--- src/popinn/__init__.py
+++ src/popinn/__init__.py
@@ -1,5 +1,5 @@
-from .models import PINN, P2INN, DeepONet
+from .models import PINN, P2INN, DeepONet, AbstractModel
 from .eval import eval_grid, eval_grid_flat_aux
 from .train import train_model, AdamConfig, LBFGSConfig
-from .loss import ResidualTerm, Loss
+from .loss import ResidualTerm, Loss, FixedWeights
 from .utils import plot_training_history
